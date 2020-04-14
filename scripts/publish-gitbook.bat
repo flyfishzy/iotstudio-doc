ffd7f186b768288ece903021fc2aebef9eb5fc5f
joey <flyfish.zy@gmail.com>
2020-04-14 15:16:57 +0800

    update windows bat
    update windows bat
    
    update bat

---
+++ scripts/publish-gitbook.bat
@@ -0,0 +1,22 @@
+:: checkout to the gh-pages branch
+git checkout gh-pages
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+:: pull the latest updates
+git pull origin gh-pages --rebase
+
+:: copy the static site files into the current directory.
+xcopy  _book/* . /e
+
+:: remove '_book' directory
+git clean -fx _book
+
+:: add all files
+git add .
+:: commit
+git commit -m 'publish gh-pages'
+:: push to the origin
+git push origin gh-pages
+
+:: checkout to the master branch
+git checkout master

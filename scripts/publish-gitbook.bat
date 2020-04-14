a89623cc5be026e27c601f63d2e542c6678508c9
joey <flyfish.zy@gmail.com>
2020-04-14 16:16:08 +0800

    publish gh-pages

--- scripts/publish-gitbook.bat
+++ scripts/publish-gitbook.bat
@@ -1,10 +1,12 @@
-
+:: checkout to the gh-pages branch
+git checkout gh-pages
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: pull the latest updates
 git pull origin gh-pages --rebase
 
 :: copy the static site files into the current directory.
-xcopy _book\*.* . /e /y
+xcopy  _book\*.* . /e /y
 
 :: remove '_book' directory
 git clean -fx _book
@@ -14,7 +16,7 @@ git add .
 :: commit
 git commit -m "publish gh-pages"
 :: push to the origin
-
+git push origin gh-pages
 
 :: checkout to the master branch
 git checkout master
